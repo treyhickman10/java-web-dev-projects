8b1f88a99a97c42e3b38bc1dac0e3f0b71513015
treyhickman10 <thickman10@gmail.com>
2024-09-10 15:07:35 -0500

    Attempt with redirect

--- src/main/java/org/launchcode/Main.java
+++ src/main/java/org/launchcode/Main.java
@@ -8,12 +8,13 @@ public class Main {
 	public static void main(String[] args) {
 		String phrase = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
 		String[] characters = phrase.split("");
-//		ArrayList<String> charArrayList = new ArrayList<>(List.of(characters));
 		HashMap<String, Integer> charCount = new HashMap<>();
 		for (String letter : characters){
 			if(!charCount.containsKey(letter)){
 				charCount.put(letter, 1);
-			}else {f}
+			}else {
+				charCount.replace(letter, charCount.get(letter) + 1);
+			}
 
 //			while(charArrayList.contains(letter)){
 //
